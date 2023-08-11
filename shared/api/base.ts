90c5a315bfd019ac9670bb96a9a39dfef412e8e4
tyc <yct.h89@gmail.com>
2023-08-11 16:31:39 +0800

    fix: encoding bugs

--- shared/api/base.ts
+++ shared/api/base.ts
@@ -1,16 +1,18 @@
 interface Option<RequestPayload, ResponsePayload> {
-  server:  (req: RequestPayload) => Promise<ResponsePayload>;
-  client:  (req: RequestPayload) => Promise<ResponsePayload>;
+  server: (req: RequestPayload) => Promise<ResponsePayload>
+  client: (req: RequestPayload) => Promise<ResponsePayload>
 }
-const createApi = <RequestPayload = unknown, ResponsePayload = unknown>(option: Option<RequestPayload, ResponsePayload>) => (option)
+const createApi = <RequestPayload = unknown, ResponsePayload = unknown>(
+  option: Option<RequestPayload, ResponsePayload>
+) => option
 
-// example api 
+// typescript connect api client and server side (ex. zod,trpc library)
 
-const api1ReqSchema = z.objcect({})
+// const api1ReqSchema = z.objcect({})
 
-export const api1 = createApi<z.infer<typeof api1ReqSchema>, {ok:boolean; message: string}>({
-  client: (data) => {
-    api1ReqSchema.parse(data)
-  },
-  server: () => {},
-})
+// export const api1 = createApi<z.infer<typeof api1ReqSchema>, {ok:boolean; message: string}>({
+//   client: (data) => {
+//     api1ReqSchema.parse(data)
+//   },
+//   server: () => {},
+// })
